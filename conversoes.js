d73912dfbd6046b28e714b0cdadfd418ece07574
arturk22 <alencar.artur00@gmail.com>
2022-03-02 19:52:12 -0300

    usando Conversões

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// conversao implicita 
+
+const numero = 456;
+const numeroString = '456';
+
+console.log(numero +  numeroString);
+
+
+
+// conversao explicita
+
+console.log(numero +  Number(numeroString));
+

d290307f5e6c69d2a360a090496342d81e95ba05
arturk22 <alencar.artur00@gmail.com>
2022-03-02 15:36:20 -0300

    usando truthy and falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,13 @@
+// truthy and falsy
+
+let minhaVar;
+let varNull =  null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = 'alura';
+
+console.log(typeof numero)
+console.log(typeof texto)
